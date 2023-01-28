138d24a6b453cef6d9e4651f00cd68b48064dba4
Shane Hingtgen <shanehingtgen@gmail.com>
2023-01-27 23:13:48 -0600

    backing up

--- index.js
+++ index.js
@@ -10,20 +10,57 @@ app.set("view engine", "ejs");
 
 // set static files
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use("/site", express.static(path.join(__dirname, "public/stylesheets")));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Pets-R-Us: Landing",
-    pageTitle: "Pets-R-Us Landing",
+    pageTitle: "Where pets are happy and healthy",
   });
 });
 
 app.get("/grooming", (req, res) => {
   res.render("grooming", {
     title: "Pets-R-Us: Grooming",
-    pageTitle: "Pets-R-Us Grooming",
+    pageTitle: "Grooming Services",
+  });
+});
+
+app.get("/boarding", (req, res) => {
+  res.render("boarding", {
+    title: "Pets-R-Us: Boarding",
+    pageTitle: "Pets-R-Us Boarding",
+  });
+});
+
+app.get("/training", (req, res) => {
+  res.render("training", {
+    title: "Pets-R-Us: Training",
+    pageTitle: "Pets-R-Us Training",
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", {
+    title: "Pets-R-Us: Register",
+    pageTitle: "Pets-R-Us Register",
+  });
+});
+
+app.get("/customers", (req, res) => {
+  res.render("customers", {
+    title: "Pets-R-Us: Customer List",
+    pageTitle: "Pets-R-Us Customer List",
+  });
+});
+
+app.get("/appointment", (req, res) => {
+  res.render("appointment", {
+    title: "Pets-R-Us: My Appointments",
+    pageTitle: "Pets-R-Us My Appointments",
   });
 });
 
