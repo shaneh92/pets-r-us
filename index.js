8388f650b399c6572db6c3a0cdd8086737b78ee4
Shane Hingtgen <shanehingtgen@gmail.com>
2023-01-28 15:00:27 -0600

    added comments and form field on index

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+/*
+==============================================================================
+; Title: index.js Pets-R-Us
+; Author: Shane Hingtgen
+; Bellevue University
+; Date: 01/28/23
+; Description: This file allows website connection via express
+; Work Cited: 
+    Web 330 HTML, CSS, and JavaScript Requirements
+    Web 340 Assign 4
+    Web 340 Assign Pets-R-Us
+    Web 340 Power points
+    Node.js Setup with express https://www.youtube.com/watch?v=A01KtJTv1oc&embeds_euri=https%3A%2F%2Fcyberactive.bellevue.edu%2F&source_ve_path=MjM4NTE&feature=emb_title
+=================================================================================================================
+*/
 "use-strict";
 //imports
 const express = require("express");
 const path = require("path");
 const app = express();
 
-//set views
+//set views and view engine as ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// set static files
+// set static files, folder: public, has images and stylesheets
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use("/site", express.static(path.join(__dirname, "public/stylesheets")));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // set the port up
 
+// These are the web pages connected
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Pets-R-Us: Landing",
